301061b12f780da961516fc8bc1a001c8fa2466f
ayroti dey sarkar <76876452+ayrotideysarkar@users.noreply.github.com>
2021-10-03 13:22:27 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,26 +1,16 @@
-#include<iostream>
-using namespace std;
-
-int Fib(int n)
-{
-    if (n == 0 || n == 1)
-    {
-        return n;
-    }
-    else {
-        return Fib(n-1) + Fib(n-2);
-    }
-}
-
-int main(){
-    int n,j=0;
-    cout << "Enter the total number of terms:";
-    cin >> n;
-
-    for(int i=1; i<=n; i++)
-    {
-            int r = Fib(j);
-            cout << r << " ";
-            j++;
-    }
-}
+#include <iostream>  
+using namespace std;  
+int main() {  
+  int n1=0,n2=1,n3,i,number;    
+ cout<<"Enter the number of elements: ";    
+ cin>>number;    
+ cout<<n1<<" "<<n2<<" "; //printing 0 and 1    
+ for(i=2;i<number;++i) //loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  cout<<n3<<" ";    
+  n1=n2;    
+  n2=n3;    
+ }    
+   return 0;  
+   }  
